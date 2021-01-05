c251ef5a19453e10af0451773b6b0c7179c5b421
yeonsookim <yeonsoo.kim@m2cloud.kr>
2021-01-05 15:31:47 +0900

    회원가입 함수 작성

--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const models = require("../models")
 const app = express.Router();
+const _ = require("lodash")
 module.exports = app;
 
-app.get('/signup', (req, res) => {
-    console.log(req.body);
+app.post('/signup', async (req, res) => {
+    const data = req.body;
+    const reqData = ['user_name', 'user_age'];
 
-    res.send({'status': 'success'})
+    // 필요한 데이터가 다 담겨있는지 확인
+    let isCheckedData = true;
+    _.each(reqData, e => {
+        if (!(e in data)) {
+            return isCheckedData = false;
+        }
+    })
+
+    // 데이터 불충분
+    if (!isCheckedData) {
+        return res.status(400).end();
+    }
+
+    models.User.create({
+        user_name: data.user_name,
+        user_age: data.user_age,
+    });
+
+    return res.send({'users': await models.User.findAll()});
 })
